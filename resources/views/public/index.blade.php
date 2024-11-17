2aa6c07f5172d0485fe3af1289b6805f01f2f702
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-11-17 12:17:29 -0500

    final

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -128,7 +128,7 @@
                             <a href="/catalogo?subcategoria={{$subcategoria->id}}">       
                                 <img class="w-full h-full object-cover aspect-square" src="{{ asset($subcategoria->url_image . $subcategoria->name_image) }}" /> 
                             </a>
-                            <div class="absolute inset-0 bg-gradient-to-t from-[#000000de] to-transparent"></div>
+                            <div class="absolute inset-0 bg-gradient-to-t from-[#00000098] to-transparent"></div>
                             <div class="flex flex-row w-full absolute bottom-5">
                                 <div class="flex flex-row justify-center items-center w-full">
                                     <h2
