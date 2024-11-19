86380c3265ee8f77173f9ee6574a32465f406bec
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-11-19 11:31:46 -0500

    final

--- resources/views/components/public/matrix.blade.php
+++ resources/views/components/public/matrix.blade.php
@@ -8,7 +8,7 @@
 <head>
     @viteReactRefresh
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>{{ env('APP_NAME') }}</title>
     <title>American Brands | Ofertas en americanbrands.pe</title>
     <meta HTTP-EQUIV="bulletin-text" content="Arma tu outfit con 50% de descuento en las mejores marcas Bronco Jeans, Lois Perú, Machine, 1921 International, Tayssir y más.">
