2c3c14b9f8cc56745ffcc4d0998ee2141c9a0d36
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-11-29 11:18:05 -0500

    final

--- resources/js/components/Filter/FilterContainer.jsx
+++ resources/js/components/Filter/FilterContainer.jsx
@@ -214,7 +214,7 @@ const FilterContainer = ({ minPrice, setFilter, filter, maxPrice, categories = [
 
 
     {
-      tags.length > 0 && <div className="flex-col gap-4 w-full flex">
+      tags.length > 0 && <div className="flex-col gap-4 w-full hidden">
         <h2 className="font-Urbanist_Semibold tracking-wide font-semibold text-base">Etiquetas</h2>
         <div className='bg-[#808080] pb-[1px] -mt-2'></div>
         <div className='flex flex-col gap-2 w-full flex-wrap font-Urbanist_Light'>
