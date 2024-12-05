c4d0504f22752a24547f511c79510eae89401d9e
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-12-05 08:56:38 -0500

    final

--- resources/views/components/sales/modal.blade.php
+++ resources/views/components/sales/modal.blade.php
@@ -14,19 +14,24 @@
       class="w-max block mx-auto text-xs font-medium px-2.5 py-0.5 mb-1 rounded-full"></span> --}}
   </div>
   <h4 class="h4 mb-2 mt-2">Orden #<span id="invoice-code"></span></h4>
-  <p id="invoice-client" class="font-bold mb-2"></p>
-  <span>Direccion Envio:</span>
-  <p id="invoice-address" class="text-gray-700 mb-2"></p>
+  <p id="invoice-client" class="mb-2 font-bold"></p>
+  <div class="flex flex-row gap-2"><span>Email: </span><p id="email-client"></p></div>
+  <div class="flex flex-row gap-2"><span>Dni/Ruc: </span><p id="dni-client" class=""></p></div>
+  <div class="flex flex-row gap-2"><span>Telefono: </span><p id="phone-client" class=""></p></div>
+  <div class="flex flex-row gap-2"><span>Direccion Envio:</span>
+  <p id="invoice-address" class="text-gray-700 mb-2"></p></div>
 
-  <p class="font-bold"> Datos Facturacion: </p>
-  <p class=" ">
-    <span>Nombre / Razon Social: </span>
-    <span id="razonS"></span>
-  </p>
-  <p class=" mb-2">
-    <span> Direccion Fiscal:</span>
-    <span id="dirFact"></span>
-  </p>
+  <div id="seccioncomprobante" class="flex flex-col gap-2">
+    <p class="font-bold"> Datos Facturacion: </p>
+    <p id="razonsocial">
+      <span>Nombre / Razon Social: </span>
+      <span id="razonS"></span>
+    </p>
+    <p id="direccion" class="mb-2">
+      <span> Direccion Fiscal:</span>
+      <span id="dirFact"></span>
+    </p>
+  </div>
 
   @if ($isAdmin)
     <div class="mb-2 flex gap-2 items-center">
@@ -108,8 +113,29 @@
     $('#invoice-id').val(data.id)
     $('#address-tipo-comprobante').text(data.tipo_comprobante.toUpperCase())
     $('#n_document').text(data.doc_number)
-    $('#razonS').text(data.razon_fact)
-    $('#dirFact').text(data.direccion_fact)
+
+
+      if (!data.razon_fact && !data.direccion_fact) {
+          $('#seccioncomprobante').hide();
+      } else { 
+          $('#seccioncomprobante').show();
+
+      if (data.razon_fact) {
+          $('#razonS').text(data.razon_fact);
+          $('#razonsocial').show(); 
+      } else {
+          $('#razonsocial').hide(); 
+      }
+
+      if (data.direccion_fact) {
+          $('#dirFact').text(data.direccion_fact);
+          $('#direccion').show(); 
+      } else {
+          $('#direccion').hide(); 
+      }
+}
+
+
     let totalInvoice = Number(data.total) + Number(envio)
     $('#invoice-price').text(totalInvoice)
     /* $('#invoice-address-price').text(isFree ? 'Envio gratis' :
@@ -122,6 +148,9 @@
       .removeClass('bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300')
     $('#invoice-code').text(data.code)
     $('#invoice-client').text(`${data.name} ${data.lastname}`)
+    $('#email-client').text(`${data.email}`)
+    $('#dni-client').text(`${data.doc_number}`)
+    $('#phone-client').text(`${data.phone}`)
     $('#invoice-address').text(data.address_description ?
       `${data.address_department}, ${data.address_province}, ${data.address_district} - ${data.address_street} #${data.address_number}` :
       'Recojo en tienda')
@@ -160,7 +189,8 @@
         data.forEach(item => {
           $('#invoice-products').append(`<tr class="bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              ${item.product_name}
+              ${item.product_name} <br>
+              ${item.product_color} - ${item.talla} 
             </th>
             <td class="px-6 py-4">
               S/. ${Number(item.price).toFixed(2)}
