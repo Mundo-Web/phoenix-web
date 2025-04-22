27ed9eb8183f1db0875b63ba20214e3ee9b0aaec
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-04-22 17:08:35 -0500

    final

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -325,7 +325,7 @@
                             <div class="flex flex-row items-center justify-start">
                                 <a href="{{route('contacto')}}">
                                     <div class="text-white font-roboto_medium flex flex-row gap-2 bg-[#FB4535] rounded-3xl text-center w-auto py-2 px-6">
-                                        ¿Tienes dudas?. Escríbenos
+                                        ¿Tienes dudas? Escríbenos
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                             <path d="M7 7H17M17 7V17M17 7L7 17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                         </svg>
