5b9416765d035428e89612c89c73db8e3559235c
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-04-27 16:15:51 -0500

    final

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -18,7 +18,7 @@ class EmailConfig
         $mail->SMTPAuth = true;
         $mail->Username = $general->city; // es mi usuario
         $mail->Password = $general->country; // es mi contraseña
-        $mail->SMTPSecure = 'tls';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         $mail->Subject = $name . ', '.$mensaje;
