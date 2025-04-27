6fa8c8e8b6622dce5db4160e7e5ee9b9179235d9
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-04-27 16:44:33 -0500

    final

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -17,14 +17,14 @@ class EmailConfig
         $mail->isSMTP();
         $mail->Host = 'smtp.office365.com';
         $mail->SMTPAuth = true;
-        $mail->Username = $general->city; // es mi usuario
-        $mail->Password = $general->country; // es mi contraseña
+        $mail->Username = 'servicioalcliente@phoenix-fitness.fit'; // es mi usuario
+        $mail->Password = 'pmrqwzkdxzyjjnzm'; // es mi contraseña
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         $mail->Subject = $name . ', '.$mensaje;
         $mail->CharSet = 'UTF-8';
-        $mail->setFrom($general->city, 'Phoenix Fitness Center');
+        $mail->setFrom('servicioalcliente@phoenix-fitness.fit', 'Phoenix Fitness Center');
         return $mail;
     }
 }
